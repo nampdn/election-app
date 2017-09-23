c4fe9a0a398d2881657262c1307a9aeea6a55058
Thien Nguyen <tatthien.contact@gmail.com>
2017-09-23 13:43:05 +0700

    Add chunk name for route components

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-const Election = () => import('@/components/pages/Election')
+const Election = () => import(/* webpackChunkName: "election" */ '@/components/pages/Election')
 
 Vue.use(Router)
 
