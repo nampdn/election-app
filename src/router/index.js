ad5868719bb273393f75e49712491c275a2a69c2
Thien Nguyen <tatthien.contact@gmail.com>
2017-09-23 13:11:21 +0700

    Add code splitting for route components

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Electron from '@/components/pages/Election'
+const Election = () => import('@/components/pages/Election')
 
 Vue.use(Router)
 
@@ -10,7 +10,7 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Electron
+      component: Election
     }
   ]
 })
