7f678756a2257b81f347301fd5da1ace733ce46b
Thien Nguyen <tatthien.contact@gmail.com>
2018-09-06 10:24:09 +0700

    Update language key for Vietnamese

--- src/lang/vi.js
+++ src/lang/vi.js
@@ -12,5 +12,5 @@ module.exports = {
   candidate_delete_notice: 'Nếu để trống đồng nghĩa với xóa ứng viên này. Bạn chắc chứ?',
   total_electors: 'Tổng số phiếu bầu',
   delete_data: 'Xóa dữ liệu',
-  delete_all_notice: 'Bạn có chắc xóa toàn bộ dữ liệu?'
+  delete_data_notice: 'Bạn có chắc xóa toàn bộ dữ liệu?'
 }
