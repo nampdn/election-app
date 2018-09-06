dfac39f7cd0082c8e3cd50d7ad284cd81e6423c0
Thien Nguyen <tatthien.contact@gmail.com>
2018-09-06 13:16:24 +0700

    Update footer

--- src/components/shared/SiteFooter.vue
+++ src/components/shared/SiteFooter.vue
@@ -2,7 +2,7 @@
   <footer id="site-footer">
     <div class="container">
       © 2017 Thien Nguyen.
-      Hosted on <a href="https://netlify.com" target="_blank">Netlify</a>
+      Hosted on <a href="https://netlify.com" target="_blank">Netlify</a> /
       Repo on <a href="https://github.com/tatthien/election-app" target="_blank">Github</a>
     </div>
   </footer><!-- ./#site-footer -->
